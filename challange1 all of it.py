5c2836c6146380ad9da49e3f9087c76bdd78d397
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-09-08 11:55:35 +0100

    Add files via upload

---
+++ challange1 all of it.py
@@ -0,0 +1,7 @@
+MyFloatValue = 9.82
+print(type(MyFloatValue))
+MyFloatValue2 =str(MyFloatValue)
+print(type(MyFloatValue2))
+
+yes="yes"
+yes =int(yes)
---
+++ new work.py
@@ -0,0 +1,10 @@
+MyFloatValue = 9.82
+print(type(MyFloatValue))
+MyFloatValue2 =str(MyFloatValue)
+print(type(MyFloatValue2))
+
+year =input("Enter the current year ")
+age =input("What age will you be at the end of this year? ")
+print("You were born in", int(year) - int(age))
+
+print("2000" + str(18))
