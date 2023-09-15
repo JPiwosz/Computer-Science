183ef5151eb380682f45a2a4cbd9f5eb23ac7bdf
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-09-15 12:30:22 +0100

    Add files via upload

---
+++ excercise 9 part 1 , 2 , 3.py
@@ -0,0 +1,27 @@
+yName = input("Please write down the name James		")
+
+zName = len(yName) 
+
+James=yName[0:1]+yName[2:3]+yName[4:5]
+print(yName)
+print(James)
+
+
+print("The Middle Names are %s" %"John")
+
+msg = "The Middle Names are %s"
+name = input("Write the first name please ")
+name1 = input("Write the second name please ")
+Z = name[4:7]
+Y = name1[2:5]
+
+print(Z + Y)
+
+msg = "The Name is %s"
+s1 = "Aunt"
+s2 = "Kelly"
+x = s1[ :2]
+q = s1[2:4]
+Namey = x +s2 + q
+print(msg%Namey)
+
