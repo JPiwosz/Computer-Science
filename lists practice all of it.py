ab96b8abd84a9094d65e8e94f5702666029f80f2
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-09-20 12:47:17 +0100

    Add files via upload

---
+++ lists practice all of it.py
@@ -0,0 +1,58 @@
+My_list = ["computer", "science"]
+My_list = ["computer", "science", 12,15.4]
+MY_list = ["computer", "science",12,15.4,["inner","list"]]
+My_list = ["compuer","science",12,15.4,["inner","list"],"computer","computer"]
+print(My_list)
+for every_item_in_this_string in "alphabet":
+    print(every_item_in_this_string)
+    My_List = ["Monday", "Tuesday" , "Wednesday"]
+    for x in My_List :
+        print(x)
+b = ["Hello","world","I","need","some","other","words",21,34.789]
+First_item = b[0]
+Third_item = b[2]
+Num_items_in_lst = len(b)
+Last_item = b[Num_items_in_lst -1]
+Last_item = b[-1]
+print(len(b))
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[2:5])
+print(thislist[ :5])
+print(thislist[2: ])
+print(thislist[ : ])
+print(thislist[-4:-1])
+b1 = "Hello , World!"
+print(b1[2:10:2])
+print(b1[2:10:3])
+list1 = ["a","b","c"]
+list2 = [1, 2, 3]
+list3= list1 + list2
+print(list3)
+b2 = ["hello"]
+output_list = b2*2
+print(output_list)
+My_string = "hello"
+My_string_upper = My_string.upper()
+print(My_string_upper)
+My_string = My_string.upper()
+print(My_string)
+thislist1 = ["apple", "banana", "cherry"]
+thislist1[1] = "blackcurrant"
+print(thislist1)
+thislist1 = ["apple","banana","cherry","orange","kiwi","mango"]
+thislist1[1:3] = ["blackcurrant", "watermelon"]
+print(thislist1)
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("yes, 'free' is present. \n")
+lst = [ 1, 6, 3, 5, 3, 4]
+if 7 in lst:
+    print("7 is in lst")
+else:
+    print("7 is not in lst")
+if "expensive" not in txt:
+    print("No, \"expensive\" is NOT present. \n")
+if 7 not in lst:
+    print("7 is not in lst")
+else:
+    print("7 is in lst")
