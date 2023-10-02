b55571738bc9dd7c194fff6ce6f02b11bc047339
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-10-02 11:56:58 +0100

    Add files via upload

---
+++ 35 and 36.py
@@ -0,0 +1,26 @@
+
+Rnum = input("Type either 1 , 2 or 3 please  ")
+Rnumnew = int(Rnum)
+print(Rnumnew)
+if Rnumnew == 1:
+    print("Thank You")
+elif Rnumnew == 2:
+    print("Well Done")
+elif Rnumnew == 3:
+    print("Correct")
+elif Rnumnew > 3 or Rnumnew <1 :
+    print("Error Message")
+    
+Username = input("Please Enter Your Name  ")
+for apples in range(3):
+    print(Username)
+    
+Username1 = input("Please Enter Your Name again   ")
+number = input("Please Enter A Number  ")
+Number1 = int(number)
+for i in range(Number1):
+    print(Username1)
+    
+name = input("Please enter your name once again  ")
+
+print(name)
