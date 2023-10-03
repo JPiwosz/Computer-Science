e66f581bb2127442491ffcbd3ecd29020c6dc5fb
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-10-03 21:10:13 +0100

    Add files via upload

---
+++ 37 done 38 half done.py
@@ -0,0 +1,20 @@
+
+name = input("Please enter your name once again  ")
+print(name)
+nameupd = len(name)
+print(nameupd)
+for d in range(nameupd):
+    print(name[d]);
+
+
+name2 = input("Please enter you name once more  ")
+num2 = input("Please enter a number  ")
+name2new = len(name2)
+num2new = int(num2)
+#for c in range(num2new):
+    #print(name2[c],end=" ");
+for f in range(name2new):
+    print(name2[f],end=" ");
+#for c in range(num2new):
+    #print(name2[f],end=" ");
+   
