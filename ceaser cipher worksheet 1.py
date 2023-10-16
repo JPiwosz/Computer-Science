58d7f817869c9e58af208c5409e164b08839da27
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-10-16 11:56:54 +0100

    Add files via upload

---
+++ ceaser cipher worksheet 1.py
@@ -0,0 +1,11 @@
+
+#DEFGHIJKLMNOPQRSTUVWXYZBC
+schoolname = "Dwkorqh FrqqxqlwB Frlldgh"
+print(schoolname)
+print("That is your school name in Caeser Cipher")
+#25 Possible keys
+#What do you get when you cross a snowman with a vampire? Frostbite
+
+
+    
+
