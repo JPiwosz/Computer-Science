6b3e48efab9139d1585c79702b965dd9fc0c2808
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-10-27 12:36:18 +0100

    Add files via upload

---
+++ 60-65.py
@@ -0,0 +1,91 @@
+import turtle
+turtle.pensize(3)
+scr = turtle.Screen()
+turtle.shape("turtle")
+"""
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.forward(80)
+turtle.left(120)
+turtle.forward(80)
+turtle.left(120)
+turtle.forward(80)
+"""
+turtle.speed(10)
+"""
+for i in range(360):
+    turtle.left(1)
+    turtle.forward(1)
+"""
+
+"""
+colourlist = ["yellow","blue","red"]
+for b in range(3):
+    turtle.color("black",colourlist[b])
+    turtle.begin_fill()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.end_fill()
+    turtle.penup()
+    if b != 2:
+        turtle.forward(150)
+        turtle.pendown()
+"""
+
+"""
+turtle.left(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.penup()
+
+turtle.forward(100)
+turtle.pendown()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.penup()
+turtle.forward(100)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(75)
+turtle.right(180)
+turtle.penup()
+turtle.forward(75)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+"""
+turtle.left(45)
+turtle.forward(150)
+turtle.left(135)
+turtle.forward(150)
+turtle.left(135)
+turtle.forward(150)
+turtle.left(145)
+turtle.forward(150)
+turtle.left(155)
+turtle.forward(150)
