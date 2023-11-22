cd5c00a8e27807c514ff0fba4f277d7ca54b74fe
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-11-22 13:12:44 +0000

    Add files via upload

--- new work.py
+++ new work.py
@@ -1,10 +1,4 @@
-MyFloatValue = 9.82
-print(type(MyFloatValue))
-MyFloatValue2 =str(MyFloatValue)
-print(type(MyFloatValue2))
-
-year =input("Enter the current year ")
-age =input("What age will you be at the end of this year? ")
-print("You were born in", int(year) - int(age))
-
-print("2000" + str(18))
+opening = open ("C:\\Users\\20JPiwosz.ACC\\Downloads\\numbers.txt",'r')
+for line  in opening:
+    print(line.strip())
+opening.close()
