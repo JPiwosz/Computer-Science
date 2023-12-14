ee5c1a6026bf641d1f12c8d4896a35f6c7d68b0a
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-12-14 21:36:04 +0000

    Add files via upload

---
+++ exam prep 1 not finished.py
@@ -0,0 +1,27 @@
+print("Hello what is your name?  ")
+username = input("What is your name?   ")
+print("Weclome",username,"!")
+
+integer1 = input("Enter your first Integer  ")
+integer2 = input("Enter your second Integer  ")
+sumofint = int(integer1) + int(integer2)
+print(sumofint)
+
+user_tempature = input("Enter an Tempature in degrees celcius  ")
+celtemp = int(user_tempature)
+fertemp = 9/5 * celtemp + 32
+print(fertemp)
+
+principle = input("Enter Princible ")
+rate = input("ENTER Rate ")
+time = input("Enter Time ")
+principleint = int(principle)
+rateint = int(rate)
+timeint = int(time)
+simple_interest = principleint * rateint * timeint / 100
+print("Simple Interest is",simple_interest)
+
+seconds = input("Enter an amount of seconds  ")
+secsint = int(seconds)
+
+
