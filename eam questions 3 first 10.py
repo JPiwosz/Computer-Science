2defcf3a06e87352be512076c84ea2daced2e565
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-12-19 22:48:57 +0000

    Add files via upload

---
+++ eam questions 3 first 10.py
@@ -0,0 +1,59 @@
+
+for num in range(1,11):
+    print(num)
+    
+numb1 = 20
+while numb1 >= 1:
+    print(numb1)
+    numb1 = numb1 - 1
+
+for numbe in range(2,11,2):
+    print(numbe)
+    
+usernum = input("Enter an Number  ")
+usernumint = int(usernum)
+usernumintN = usernumint + 1
+for i in range(1,usernumintN):
+    print(i)
+    
+usernumb = input("Enter an Number   ")
+usernumbint = int(usernumb)
+usernumbintN = usernumbint + 1
+for E in range(1,usernumbintN,2):
+    print(E)
+    
+Text = "Happy Birthay"
+for wish in range(5):
+    print(Text)
+    
+N = input("Enter an Number  ")
+Nint = int(N)
+NintN = Nint + 1
+for ii in range(1,NintN):      
+    newN  = ii ** 2
+    print(newN)
+    
+"""    
+this also works for the above question:
+    newN = ii * ii
+    print(newN)
+"""
+
+Number = input("Enter an Number  ")
+Numberint = int(Number)
+for iii in range(1,13):
+    Answer = Numberint * iii
+    print(Answer,"=",Numberint,"*",iii)
+    
+start = 3
+difference = 4
+for iv  in range(8):
+    term = start + iv * difference
+    print(term)
+    
+start1 = 2
+ratio = 3
+for v in range(6):
+    term1 = start1 * (ratio**v)
+    print(term1)
+    
