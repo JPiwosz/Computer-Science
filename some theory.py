1e4e28fdf7bad2c3bb12a6ee38207a78b61f34fc
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2023-12-19 23:56:16 +0000

    Add files via upload

---
+++ some theory.py
@@ -0,0 +1,22 @@
+a = "welcome" + "10"
+print(a)
+
+"""
+b = "welcome" + 10
+print(b)
+"""
+
+"""
+L = [10,20,30.40,50]
+L = L + 5
+print(L)
+TypeError: can only concatenate list (not "int") to list
+"""
+L = []
+for i in range (4):
+ L.append(2 * i + 1)
+print (L[::-1])
+ 
+ asnwer = [7.5.3.1]
+ 
+ 
