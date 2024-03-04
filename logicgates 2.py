89ffcbebabddf0fd81411e14bfdffb059c59edbb
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2024-03-04 11:49:26 +0000

    Add files via upload

---
+++ logicgates 2.py
@@ -0,0 +1,54 @@
+def Not_Gate (a):
+    if a == 1:
+        return 0
+    else:
+        return 1
+        
+def And_Gate (a,b):
+    if a == 1 and b == 1:
+        return 1
+    else:
+        return 0
+
+def Or_Gate (a,b):
+    if a or b == 1:
+        return 1
+    else:
+        return 0
+    
+def Xor_Gate (a,b):
+    if a == 1 and b == 1:
+        return 0
+    if a == 1 and b == 0:
+        return 1
+    if a == 0 and b == 1:
+        return 1
+    else:
+        return 0
+def Nand_Gate (a,b):
+    if a == 1 and b == 1:
+        return 0
+    if a == 0 and b == 0:
+        return 1
+    else:
+        return 0
+    
+def Nor_Gate (a,b):
+    if a == 0 and b == 0:
+        return 1
+    else:
+        return 0
+    
+def Xnor_Gate (a,b):
+    if a == 0 and b == 0:
+        return 1
+    if a == 1 and b == 1:
+        return 1
+    else:
+        return 0
+
+
+    
+
+
+
