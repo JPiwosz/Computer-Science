8245f58bb70c4a88d85e8a7e40b0529eddb241dd
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2024-04-12 12:32:38 +0100

    Add files via upload

---
+++ flow chart 1.py
@@ -0,0 +1,9 @@
+print("Press ''y'' to start")
+start = input("----> ")
+if start == "y":
+    inptemp = input("Enter Tempature in degrees C \n----> ")
+    f = 9.0/5.0
+    F = f
+    fer = (int(inptemp) * F) + 32
+    print("That is",fer,"in degrees fahrenheit")
+   
