cb0d12129715d2ba57fe40a52a868fd008d6b488
JPiwosz <144443601+JPiwosz@users.noreply.github.com>
2024-11-04 12:00:19 +0000

    Add files via upload

---
+++ JAVASCRIPT.js
@@ -0,0 +1,41 @@
+console.log("Hello World");
+// comments are ignored by the computer
+// two forward slashes are single line comments
+/*
+This is a
+multi-line comment
+*/
+//You can store data in variables
+//To declare a variable use the let keyword
+let myVariable =  5;
+console.log(myVariable);
+//text is written in quotes
+let myName = 'John';
+let myOtherName = 'Doe';
+console.log(myName,myOtherName);
+//variables are case sensative
+let myname = 'Bob'; //This is different to myName
+console.log(myName, myname);
+//variables must start with a letter [a-zA-Z]
+//underscore _ or dollar sign $
+$some_variable = 200;
+_someotherVariable = 500
+total = $some_variable + _someotherVariable;
+console.log(total);
+//math operators are like ordinary math
+//+ add
+// - subtract
+// * multiply
+// / divide
+// % remainder
+
+let addition = 4+4;
+let subtraction = 5-4;
+let multiply = 4*3;
+let divide = 4/2;
+//remainder gives the remainder after division
+let remainder = 6%4;
+//The exponent or power is **
+let four_cubed = 4**3;
+//The increment operator ++
+let x = 5;
---
+++ JavaScript Introduction Excercises.js
@@ -0,0 +1,42 @@
+//Java Script introduction excercises
+
+//1
+let ver1 = 7;
+let ver2 = 8.5;
+let ver3 = 9;
+console.log(ver1,ver2,ver3);
+
+let ver1new = ver1 + ver2;
+console.log(ver1new);
+
+let ver2new = ver2 + ver3;
+console.log(ver2new);
+
+console.log(ver1new,ver2new,ver3);
+
+//2
+let n = 1;
+let answer = (n)*(n+1)*(n+2)
+console.log(answer);
+
+console.log(n,n+1,n+2);
+
+//3
+let radius = 2;
+let pi = 3.14;
+
+let area = (pi)*(radius**2)
+console.log(area);
+
+//4
+let base = 4;
+let height = 6;
+let half = 1/2
+
+let areaT = (half)*(base*height)
+console.log(areaT)
+
+//5
+let n = 2;
+
+
